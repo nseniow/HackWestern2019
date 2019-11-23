fcfa437411a29528036b0686d56eee0f5d115550
Alexander Yang <a.yang7005@gmail.com>
2019-11-23 12:54:08 -0500

    Added camera to tangerine

--- app/src/main/java/com/example/hackwestern19/FakeScreen/TangerineActivity.java
+++ app/src/main/java/com/example/hackwestern19/FakeScreen/TangerineActivity.java
@@ -4,19 +4,24 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.example.hackwestern19.PictureTaker.Camera2BasicFragment;
 import com.example.hackwestern19.R;
 
 public class TangerineActivity extends FakeActivity {
+
+    Camera2BasicFragment camera;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tangerine);
+        initializeCamera();
         initializeFakeListeners();
     }
 
@@ -29,6 +34,14 @@ public class TangerineActivity extends FakeActivity {
                 startActivity(browserIntent);
             }
         });
+
+        TextView fakeLogin= findViewById(R.id.fakeLogin);
+        fakeLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                camera.takePicture();
+            }
+        });
     }
 
     @Override
@@ -38,7 +51,8 @@ public class TangerineActivity extends FakeActivity {
 
     @Override
     void initializeCamera() {
-
+        camera = Camera2BasicFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().replace(R.id.cameraFrame, camera).commit();
     }
 
     @Override
