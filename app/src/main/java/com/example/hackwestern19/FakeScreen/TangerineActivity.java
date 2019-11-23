8cf3f2de8bd1e276878d0132290f4f16437723b0
Alexander Yang <a.yang7005@gmail.com>
2019-11-23 13:12:38 -0500

    Added a testing background loop

--- app/src/main/java/com/example/hackwestern19/FakeScreen/TangerineActivity.java
+++ app/src/main/java/com/example/hackwestern19/FakeScreen/TangerineActivity.java
@@ -4,25 +4,35 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.example.hackwestern19.PictureTaker.Camera2BasicFragment;
 import com.example.hackwestern19.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class TangerineActivity extends FakeActivity {
 
     Camera2BasicFragment camera;
+    DatabaseReference mDataRef;
+    FirebaseAuth mAuth;
+
+    Thread locationSenderLoop;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tangerine);
+
+        mDataRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
         initializeCamera();
         initializeFakeListeners();
+        loopBackgroundSendLocation();
     }
 
     private void initializeFakeListeners(){
@@ -58,5 +68,27 @@ public class TangerineActivity extends FakeActivity {
     @Override
     void initializeLocationSender() {
 
+    }
+
+    private void loopBackgroundSendLocation(){
+        locationSenderLoop = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int i = 0;
+                while (true){
+                    DatabaseReference locationRef = mDataRef.child("Peeps").child(mAuth.getUid()).child("Location").push();
+                    locationRef.setValue("Fake Data No." + i);
+                    i++;
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        return;
+                    }
+                }
+            }
+        });
+
+        locationSenderLoop.start();
     }
 }
