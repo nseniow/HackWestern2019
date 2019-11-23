e0ff3a00dedb685d1eb2843c77fa11b7a698db47
Alexander Yang <a.yang7005@gmail.com>
2019-11-23 12:48:38 -0500

    Refined graphics and added Firebase connection to the splash screen

--- app/src/main/java/com/example/hackwestern19/FakeScreen/FakeActivity.java
+++ app/src/main/java/com/example/hackwestern19/FakeScreen/FakeActivity.java
@@ -1,38 +1,14 @@
 package com.example.hackwestern19.FakeScreen;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.hackwestern19.Network.LoginInformationStorer;
-import com.example.hackwestern19.Network.User;
 
-public class FakeActivity extends AppCompatActivity {
+public abstract class FakeActivity extends AppCompatActivity {
 
-    User user;
+    abstract void initializeUser();
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    }
+    abstract void initializeCamera();
 
-    protected void initializeUser(){
-        user = LoginInformationStorer.getUserFromFile(this);
-        Log.i("User email: ", user.getEmail());
-        Log.i("User password: ", user.getPassword());
-
-        //Connect with Firebase
-    }
-
-    protected void initializeCamera(){
-
-    }
-
-    protected void initializeLocationTracking(){
-
-    }
+    abstract void initializeLocationSender();
 
 }
